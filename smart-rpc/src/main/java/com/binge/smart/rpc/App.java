d90fe9035ffffdb1fa4f7c392fbd4cee0162b6b5
wangjianbin <wangjianbin@onemt.com.cn>
2017-10-12 13:00:05 +0800

    init distribute project

---
+++ smart-rpc/src/main/java/com/binge/smart/rpc/App.java
@@ -0,0 +1,13 @@
+package com.binge.smart.rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
